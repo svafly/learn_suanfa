a8e8c7a4830cc066969a6e293c5eb22a1ba53292
svafly <svafly@126.com>
2021-09-24 19:14:19 +0800

    找规律配套

--- findPattern/16_15_master_mind.php
+++ findPattern/16_15_master_mind.php
@@ -14,8 +14,36 @@ class Solution {
             if ($solution[$i] == $guess[$i]) {
                 $m++;
             } else {
+                //这里要注意，先算比较符，再做自增运算
                 if ($dp[$solution[$i]]++ < 0)$n++;
                 if ($dp[$guess[$i]]-- > 0)$n++;
+            }
+        }
+        return [$m,$n];
+    }
+
+    function masterMind2($solution, $guess) {
+        $m = 0;
+        $n = 0;
+        $solve=[0,0,0,0];
+        $fakeSolve=[0,0,0,0];
+        //记录猜中
+        for ($i=0;$i<strlen($solution);$i++) {
+            if ($solution[$i] == $guess[$i]) {
+                $m++;
+                $solve[$i]=1;
+                $fakeSolve[$i]=1;
+            }
+        }
+        //伪猜中
+        for ($i=0;$i<strlen($solution);$i++) {
+            if ($solve[$i]) continue;
+            for ($j=0;$j<strlen($guess);$j++) {
+                if ($solution[$i] == $guess[$j] && !$fakeSolve[$j]){
+                    $n++;
+                    $fakeSolve[$j] = 1;
+                    break;
+                }
             }
         }
         return [$m,$n];
@@ -24,6 +52,6 @@ class Solution {
 $solution = new Solution();
 $f = "BGBG";
 $l = "RGBR";
-print_r($solution->masterMind($f,$l));
+print_r($solution->masterMind2($f,$l));
 //珠玑妙算
 //伪猜中那里真是绝了，solution自增，和猜测的互相抵消

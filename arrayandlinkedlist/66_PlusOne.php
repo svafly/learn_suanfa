7929d49083bce240aedddadf0d38936c24cbfe9c
svafly <svafly@126.com>
2021-10-06 20:34:06 +0800

    数组和链表

--- arrayandlinkedlist/66_PlusOne.php
+++ arrayandlinkedlist/66_PlusOne.php
@@ -14,12 +14,15 @@ class Solution {
             }
             $digits[$i]=0;
         }
-        print_r($digits);
+//        print_r($digits);
         array_unshift($digits,1);
-        print_r($digits);exit;
+//        print_r($digits);exit;
         return $digits;
     }
 }
 $solution = new Solution();
 $ar = [9];
 print_r($solution->plusOne($ar));
+//这里有个9的情况，要进1，php特殊的是数组不需要定长，用自带函数插入即可，其他语言需要新增一个位置
+//int[] newNumber = new int [len+1];
+// newNumber[0] = 1;
